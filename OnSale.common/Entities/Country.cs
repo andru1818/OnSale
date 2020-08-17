50137193999a32d9beea5766d5846c1becaddc53
ANDRES FELIPE ARISTIZABAL VELASQUEZ <andresaristizabal22119@correo.itm.edu.co>
2020-08-17 18:36:11 -0500

    Input Countries CRUD

---
+++ OnSale.common/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace OnSale.common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
